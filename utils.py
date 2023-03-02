9891efe2702dc934f5e9cf2fa0b0be503c8986ed
xysong <xysong25@gmail.com>
2023-03-02 11:04:30 -0500

    Update: two simple utility function

--- utils.py
+++ utils.py
@@ -1,3 +1,5 @@
+from secrets import choice
+from sklearn.utils import shuffle
 import torch
 import numpy as np
 
@@ -23,3 +25,35 @@ MPI_SCORE = {
 # MBTI IDX-ANSWER-SCORE CONVERSION #
 # -------------------------------- #
 # ...
+
+
+# ----------------- #
+# UTILITY FUNCTIONS #
+# ----------------- #
+
+def shuffle_choice(choice_lst):
+    assert type(
+        choice_lst) == np.ndarray, 'Please format your choice list as numpy array.'
+    assert len(np.shape(choice_lst)) == 1
+    assert len(choice_lst) > 1
+    n = len(choice_lst)
+    rand_idx = np.random.choice(n, n, replace=False)
+    return choice_lst[rand_idx]
+
+
+def ordered_lst_to_str(ordered_lst, style='mpi'):
+    if style == 'mpi':
+        option = "\nOptions: "
+        for choice in ordered_lst:
+            option += f"\n{choice}"  # Prepend \n as designed choice
+        return option + "\nAnswer: "
+    else:
+        assert False, 'Unrecognized option style.'
+
+
+# Simple testing code
+choice_lst = np.array(MPI_IDX_TO_KEY)
+ordered_lst = shuffle_choice(choice_lst)
+option = ordered_lst_to_str(ordered_lst)
+print(ordered_lst)
+print(option)
