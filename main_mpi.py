bb98814175808e28a4854a2623a7048450cc22a8
xysong <xysong25@gmail.com>
2023-03-29 21:09:13 -0400

    Update: Partial Experimental Results and os.makedirs()

--- main_mpi.py
+++ main_mpi.py
@@ -2,8 +2,8 @@ from MPI.mpi import *
 from Model.lm import *
 from Model.template import *
 import yaml
-import random
 import argparse
+import os
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 # Parse Input Arguments
@@ -24,10 +24,15 @@ path = config['path']
 dset_dir = path['dset_dir']
 ckpt_dir = path['mpi_ckpt_dir']
 log_dir = path['mpi_log_dir']
+os.makedirs(log_dir, exist_ok=True)
+os.makedirs(ckpt_dir, exist_ok=True)
+
 #####  Dataset  #####
 dataset = config['dataset']
 dset, start, end = dataset['dset'], dataset['start'], dataset['end']
 path_to_dset = dset_dir + f"{dset}.csv"
+
+
 #####  Prompt & Answer Template  #####
 tmp = config['template']
 prompt = PROMPT_TEMPLATE[tmp['prompt']]
@@ -47,13 +52,19 @@ if order_name is not None:
     shuffle_both = config['shuffle']['shuffle_both']
 else:
     order, shuffle_both = None, None
+
+
 #####  Model & Tokenizer  #####
 model_config = config['model']
 family, version = model_config['family'], model_config['version']
 model = MODEL[family].from_pretrained(version, is_decoder=False)
 tokenizer = TOKENIZER[family].from_pretrained(version)
+
+
 #####  Likelihood calculation algorithm  #####
 ll_type = config['algorithm']['ll_type']
+
+
 #####  logging filename  #####
 filename = log_fname(dset, model_config, tmp['description'])
 if args.tag:
@@ -62,8 +73,9 @@ if args.tag:
 verbose = args.verbose
 if order_name is not None:
     filename += f'_[{order_name}]'
-    # ----------------------------------- #
-    # ---------------- RUN -------------- #
+
+# ----------------------------------- #
+# ---------------- RUN -------------- #
 mpi = MPI(path_to_dset, start, end,
           prompt, index, desc, ans_type, order, shuffle_both)
 mpi.reset()
