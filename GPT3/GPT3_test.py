07029968b9ecc4971a008b68faf0396862bb11f4
kmohebbizadeh <67389272+kmohebbizadeh@users.noreply.github.com>
2023-03-30 17:06:04 -0400

    gpt 3 skeleton

---
+++ GPT3/GPT3_test.py
@@ -0,0 +1,16 @@
+import openai
+
+openai.api_key = open('api_key.txt').read()
+
+def gpt3(prompt):
+    response = openai.Completion.create(
+      model="text-curie-001",
+      prompt=prompt,
+      temperature=0.7,
+      max_tokens=64,
+      top_p=1,
+      frequency_penalty=0,
+      presence_penalty=0
+    )
+    res = response['choices'][0]['text'].strip()
+    return res
