cfaacedf8e41f315577a925623953163732fdb0c
xysong <xysong25@gmail.com>
2023-04-07 18:38:16 -0400

    Add: serialization files for productivity

---
+++ run.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# whoami
+# date
+COLOR="\e[31m"
+ENDCOLOR="\e[0m"
+LINE=$(printf "%60s")
+echo -e $(printf "${COLOR}${LINE// /=}${ENDCOLOR}")
+echo -e $(printf "${COLOR}JOB STARTED!!!${ENDCOLOR}")
+# DO NOT MODIFY ABOVE
+
+# TO RUN: change the following as needed:
+REGIME="Constraint"
+# REGIME="Open-Vocab" # uncomment this if necessary
+TYPE="order-symmetry"
+MODEL="BERT-Base" #
+driver="non-index.yaml"
+
+# Declare file path & Driver files
+FILE="config/${REGIME}/${TYPE}/${MODEL}/${driver}"
+script="main_mpi.py"
+# echo $FILE
+declare -a ORDERS=("original" "reverse" "order-I" "order-II" "order-III")
+for ((i=0; i<${#ORDERS[@]}; i++)); do
+    echo -e $(printf "${COLOR}${LINE// /=}${ENDCOLOR}")
+    echo -e $(printf "${COLOR}CURRENT RUNNING ORDER: ${ORDERS[i]}${ENDCOLOR}")
+    echo -e $(printf "${COLOR}${LINE// /=}${ENDCOLOR}")
+    yaml-set $FILE --change='/shuffle/order' --value=${ORDERS[i]}
+    python3 $script --config=$FILE
+done
+
+echo -e $(printf "${COLOR}${LINE// /=}${ENDCOLOR}")
+echo -e $(printf "${COLOR}JOB FINISHED!!!${ENDCOLOR}")
+echo -e $(printf "${COLOR}${LINE// /=}${ENDCOLOR}")
