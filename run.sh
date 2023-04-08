21d3be03d31df1d386ecfb2b0df5494298ae42c7
xysong <xysong25@gmail.com>
2023-04-08 11:43:32 -0400

    Update: run.sh for testing

--- run.sh
+++ run.sh
@@ -12,7 +12,7 @@ echo -e $(printf "${COLOR}JOB STARTED!!!${ENDCOLOR}")
 REGIME="Constraint"
 # REGIME="Open-Vocab" # uncomment this if necessary
 TYPE="order-symmetry"
-MODEL="GPT2-XL" #
+MODEL="GPT2-Large" #
 driver="non-index.yaml"
 
 # Declare file path & Driver files
