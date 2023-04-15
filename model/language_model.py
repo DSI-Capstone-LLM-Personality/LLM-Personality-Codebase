a44967527ac425f662b3f04f1f47ae41b3057eb1
xysong <xysong25@gmail.com>
2023-04-14 21:53:14 -0400

    Update: sanity check

--- model/language_model.py
+++ model/language_model.py
@@ -166,9 +166,10 @@ class PROMPTER():
                                            top_p=self.g_config['top_p'],
                                            temperature=self.g_config['temperature'],
                                            max_new_tokens=self.g_config['max_tokens'])
-            output = self.tokenizer.decode(
-                response[0][len(input_ids[0]):])  # only get new tokens
-            # output = self.tokenizer.decode(response[0])
+            # output = self.tokenizer.decode(
+            # response[0][len(input_ids[0]):])  # only get new tokens
+            print(len(response[0][len(input_ids[0]):]))
+            output = self.tokenizer.decode(response[0])
             return output
         elif self.family in ["FLAN-T5"]:
             inputs = self.tokenizer(prompt, return_tensors='pt')
