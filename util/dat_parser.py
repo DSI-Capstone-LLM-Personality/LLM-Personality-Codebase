a0c844df86afd6a76b616e86e665900eee33df50
xysong <xysong25@gmail.com>
2023-04-18 01:02:01 -0400

    Update: human dataset

---
+++ util/dat_parser.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import numpy as np
+import pyreadstat
+import os
+import time
+
+start = time.time()
+df, meta = pyreadstat.read_por("Dataset/IPIP120.por")
+print(df.head())
+df.to_csv("processed.csv")
+stop = time.time()
+print(
+    f"Processing Time: {np.round(stop - start, 2)}s | About {np.round((stop-start)/60, 1)} mins")
