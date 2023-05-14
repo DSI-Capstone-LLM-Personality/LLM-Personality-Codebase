a25b75d95591edd155195cafa734da72729bea13
xysong <xysong25@gmail.com>
2023-05-14 17:36:43 -0400

    Update: results

--- human/plot.sh
+++ human/plot.sh
@@ -10,9 +10,20 @@
 
 
 # OPT - non-index
-python human/stats.py --config=config/Constraint/order-symmetry/OPT-125M/non-index.yaml
-python human/stats.py --config=config/Constraint/order-symmetry/OPT-350M/non-index.yaml
-python human/stats.py --config=config/Constraint/order-symmetry/OPT-1.3B/non-index.yaml
-python human/stats.py --config=config/Constraint/order-symmetry/OPT-2.7B/non-index.yaml
-python human/stats.py --config=config/Constraint/order-symmetry/OPT-6.7B/non-index.yaml
-python human/stats.py --config=config/Constraint/order-symmetry/OPT-13B/non-index.yaml
+# python human/stats.py --config=config/Constraint/order-symmetry/OPT-125M/non-index.yaml
+# python human/stats.py --config=config/Constraint/order-symmetry/OPT-350M/non-index.yaml
+# python human/stats.py --config=config/Constraint/order-symmetry/OPT-1.3B/non-index.yaml
+# python human/stats.py --config=config/Constraint/order-symmetry/OPT-2.7B/non-index.yaml
+# python human/stats.py --config=config/Constraint/order-symmetry/OPT-6.7B/non-index.yaml
+# python human/stats.py --config=config/Constraint/order-symmetry/OPT-13B/non-index.yaml
+
+
+# GPT2 non-index
+python human/stats.py --config=config/Constraint/order-symmetry/GPT2/non-index.yaml
+python human/stats.py --config=config/Constraint/order-symmetry/GPT2-Medium/non-index.yaml
+python human/stats.py --config=config/Constraint/order-symmetry/GPT2-Large/non-index.yaml
+python human/stats.py --config=config/Constraint/order-symmetry/GPT2-XL/non-index.yaml
+python human/stats.py --config=config/Constraint/order-symmetry/GPTNEO-1.3B/non-index.yaml
+python human/stats.py --config=config/Constraint/order-symmetry/GPTNEO-2.7B/non-index.yaml
+python human/stats.py --config=config/Constraint/order-symmetry/GPTNEO-125M/non-index.yaml
+python human/stats.py --config=config/Constraint/order-symmetry/GPTNEOX/non-index.yaml
