0631b92ae5590929f76a2987de7b763c6588b803
xysong <xysong25@gmail.com>
2023-05-14 12:51:02 -0400

    Update: calibration code (requires cleaning)

--- model/language_model.py
+++ model/language_model.py
@@ -122,15 +122,20 @@ class LMPROB():
         self.half_precision = half_precision
 
 
-    def __call__(self, prompt, choice):
+    def __call__(self, prompt, choice, tmp):
         # ic(prompt)
         # ic(choice)
         tokens = self.tokenizer(
-            prompt + choice, return_tensors="pt", add_special_tokens=False)
+            prompt.strip() + " " + choice, return_tensors="pt", add_special_tokens=False)
         # print(prompt + choice)
+        # Calibration
+        # print(tmp)
+        tmp = tmp.strip() + " " + choice
+        tmp_tokens = self.tokenizer(tmp, return_tensors="pt", add_special_tokens=False)
+        
         # Not used: normalization
-        answer = "Answer: " + choice
-        ans_tokens = self.tokenizer(answer, return_tensors="pt", add_special_tokens=False)
+        # answer = "Answer: " + choice
+        # ans_tokens = self.tokenizer(answer, return_tensors="pt", add_special_tokens=False)
 
         for item, _ in tokens.items():
             tokens[item] = tokens[item].to(DEVICE)
@@ -139,37 +144,50 @@ class LMPROB():
             # token sequence processing
             logits = self.model(**tokens).logits
             # Not used: normalization method
-            ans_logits = self.model(**ans_tokens).logits
+            # ans_logits = self.model(**ans_tokens).logits
             # ic(logits.shape)
+            tmp_logits = self.model(**tmp_tokens).logits
+
             if self.ll_type == 'ans_inv_perp':
                 # ic(choice)
                 toi = find_critical_phrase(
                     self.tokenizer, tokens.input_ids, choice)
                 
-                ans_toi = find_critical_phrase(
-                    self.tokenizer, ans_tokens.input_ids, choice)
+                # ans_toi = find_critical_phrase(
+                #     self.tokenizer, ans_tokens.input_ids, choice)
+                
+                tmp_toi = find_critical_phrase(
+                    self.tokenizer, tmp_tokens.input_ids, choice)
 
             elif self.ll_type == 'sent_inv_perp':
                 toi = tokens.input_ids[0]
             else:
                 assert False, 'Unrecognized input argument.'
-            # extract probability and inverse perplexity
-            probs = logit_to_prob(
-                logits.squeeze(), tokens.input_ids[0])[-len(toi):]
-            # Not used: normalization
-            ans_probs = logit_to_prob(ans_logits.squeeze(), ans_tokens.input_ids[0])[-len(ans_toi):]
-            
-            # ic(self.tokenizer.decode(toi[-1]))
-            # ic(len(toi))
-            # ic(choice)
+
+            # calibration
+            prob = torch.softmax(logits.squeeze(), dim=-1)[-len(toi):]
+            tmp_prob = torch.softmax(tmp_logits.squeeze(), dim=-1)[-len(tmp_toi):]
+            # ic(prob.shape)
+            # ic(tmp_prob.shape)
+            probs = prob / tmp_prob
+            # ic(probs)
+            probs = probs / torch.sum(probs, dim=-1, keepdim=True)
             # ic(probs)
             # ic(probs.shape)
-            # ic(probs)
-            # ic(ans_probs)
-            # ic(torch.log(torch.prod(probs) / torch.prod(ans_probs)))
+            ids = tokens.input_ids[0][-len(tmp_toi):]
+            probs = probs[np.arange(ids.shape[0]), ids]
+            # ic(probs.shape)
+            # extract probability and inverse perplexity
+            # probs = logit_to_prob(
+            #     logits.squeeze(), tokens.input_ids[0])[-len(toi):]
+            # Not used: normalization
+            # ans_probs = logit_to_prob(ans_logits.squeeze(), ans_tokens.input_ids[0])[-len(ans_toi):]
+            
+            tmp_probs = logit_to_prob(tmp_logits.squeeze(), tmp_tokens.input_ids[0])[-len(tmp_toi):]
+            # PREVIOUS
             ll = prob_to_ll(probs) 
             # This is the code for normalization (not used)
-            # ll = torch.sum(torch.log(probs)) - torch.sum(torch.log(ans_probs))
+            # ll = torch.sum(torch.log(probs)) - torch.sum(torch.log(tmp_probs))
             # ic(ll)
             # ic(np.exp(-ll))
             # ic(torch.prod(probs)**(-1 / len(probs)))
@@ -239,3 +257,4 @@ class PROMPTER():
             return output
         else:
             assert False, 'Unrecognized model Type.'
+
